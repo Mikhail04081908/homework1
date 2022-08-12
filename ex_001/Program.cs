77b0dab8137c159b2474dc78710dc97c3e88d684
Kaluzhin <fk2090@gmail.com>
2022-08-12 16:23:59 +0300

    Нахождение максимального числа из 2 - х

---
+++ ex_001/Program.cs
@@ -0,0 +1,7 @@
+﻿
+// Нахождение максимального числа из 2-х
+int num1 = new Random().Next(1, 100);
+int num2 = new Random().Next(1, 100);
+int max = num1;
+if(num2 > max)max = num2;
+Console.Write($"Максимальное число из {num1} и {num2} - {max}");

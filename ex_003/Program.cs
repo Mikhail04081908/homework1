59cccbe0539bfed5c6077e6490f82fbddb74a352
Kaluzhin <fk2090@gmail.com>
2022-08-12 16:28:42 +0300

    Нахождение чётности числа

---
+++ ex_003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Нахождение чётности числа
+
+int num = new Random().Next(1, 100);
+int del = num % 2;
+
+if (del == 0)
+{
+    Console.WriteLine($"Число: {num} - Чётное");
+}
+else 
+{
+    Console.WriteLine($"Число: {num} - Не чётное");
+}

0b2430e90b518ef2ac73d93f3f47a470a54553d0
Kaluzhin <fk2090@gmail.com>
2022-08-12 16:31:31 +0300

    Числа делящиеся на 2 до (N)

---
+++ ex_004/Program.cs
@@ -0,0 +1,14 @@
+﻿// Нахождение чисел которые делятся на 2 до (N)
+
+int num = new Random().Next(1, 10);
+int max = 1;
+Console.WriteLine($"{num} - число которое было вязто");
+while(max <= num)
+{
+    if(max % 2 == 0 )
+    {
+        Console.Write($"{max},");
+    }
+    max = max + 1;
+}
+Console.Write($"числа делящиеся на 2 до числа {num}.");
